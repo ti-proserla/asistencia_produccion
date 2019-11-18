1d62a2790abfb47aa203932ff758e812870c315a
Diego <diegomf.mendoza@gmail.com>
2019-11-18 18:09:34 -0500

    Realizado

--- reporte_nisira.sql
+++ reporte_nisira.sql
@@ -1,7 +1,8 @@
 SELECT 
 	operador.dni,
 	YEAR(ingreso) as year,
-	WEEK(ingreso) as month, 
+	WEEK(ingreso) as month,
+	labor.nom_labor,
 	SUM( CASE WHEN DAYOFWEEK(ingreso)=2 THEN (TIMESTAMPDIFF(MINUTE,ingreso,salida)/60) ELSE 0 END) as Lunes, 
 	SUM( CASE WHEN DAYOFWEEK(ingreso)=3 THEN (TIMESTAMPDIFF(MINUTE,ingreso,salida)/60) ELSE 0 END) as Martes, 
 	SUM( CASE WHEN DAYOFWEEK(ingreso)=4 THEN (TIMESTAMPDIFF(MINUTE,ingreso,salida)/60) ELSE 0 END) as Miercoles, 
@@ -10,4 +11,6 @@ SELECT
 	SUM( CASE WHEN DAYOFWEEK(ingreso)=7 THEN (TIMESTAMPDIFF(MINUTE,ingreso,salida)/60) ELSE 0 END) as Sabado,
 	SUM( CASE WHEN DAYOFWEEK(ingreso)=1 THEN (TIMESTAMPDIFF(MINUTE,ingreso,salida)/60) ELSE 0 END) as Domingo
 FROM operador inner join marcador on operador.id=marcador.operador_id
+LEFT JOIN tareo on tareo.operador_id=operador.id
+left JOIN labor on labor.id=tareo.labor_id
 GROUP BY operador.dni
