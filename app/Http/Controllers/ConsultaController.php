bb0a27abd25ad3cef6fd6797547aa734b3ad8528
Diego Mendoza <diegomf.mendoza@gmail.com>
2019-12-14 05:39:50 -0500

    avance

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class ConsultaController extends Controller
 {
     public function marcas(Request $request){
-        
+        $resultado=Operador::join('marcador','operador.id','=','marcador.operador_id')
+            ->leftJoin(DB::raw('(SELECT * FROM tareo GROUP BY operador_id,turno_id) AS T'),function($join){
+                $join->on('T.operador_id', '=', 'marcador.operador_id');
+                $join->on('T.turno_id', '=', 'marcador.turno_id');
+            })
+            ->leftJoin('labor','labor.id','=','T.labor_id')
+            ->leftJoin('area','area.id','=','labor.area_id')
+            ->leftJoin('proceso','proceso.id','=','T.proceso_id')
+            ->select(DB::raw("operador.dni, CONCAT(operador.nom_operador,' ',operador.ape_operador) As NombreApellido, CONCAT(YEAR(ingreso),MONTH(ingreso),'-',WEEK(ingreso,3)) AS periodo, area.codigo As codActividad, labor.codigo AS codLabor, proceso.codigo As codProceso, labor.nom_labor,ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=2 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Lunes, ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=3 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Martes, ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=4 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Miercoles, ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=5 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Jueves, ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=6 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Viernes, ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=7 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Sabado,ROUND(SUM( CASE WHEN DAYOFWEEK(ingreso)=1 THEN (TIMESTAMPDIFF(MINUTE,ingreso,IF(salida is null,ingreso,salida))/60) ELSE 0 END),2) as Domingo"))
+            ->groupBy('dni','procesos.operador.nom_operador','procesos.operador.ape_operador',DB::raw('DATE(ingreso)'))
+            ->where(DB::raw('WEEK(ingreso,3)'),$request->week)
+            ->where(DB::raw('YEAR(ingreso)'),$request->year)
+            ->where(DB::raw('CONCAT(nom_operador,ape_operador)'),'like','%'.$request->search.'%')
+            ->paginate(8);
+        return response()->json($resultado);
     }
 }
