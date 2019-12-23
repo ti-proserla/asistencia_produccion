9c13cceb8eb2001fdbefcabddbf86cd844d34ba2
Diego <diegomf.mendoza@gmail.com>
2019-12-23 11:16:06 -0500

    headert

--- app/Http/Controllers/OperadorController.php
+++ app/Http/Controllers/OperadorController.php
@@ -67,6 +67,9 @@ class OperadorController extends Controller
         
     public function update(OperadorEditar $request, $id)
     {
+        header('Content-Type: text/html; charset=UTF-8');
+        mb_internal_encoding('UTF-8');
+        mb_http_output('UTF-8');
         mb_http_input('UTF-8');
         dd($_REQUEST['ape_operador']);
         dd($request->all());
