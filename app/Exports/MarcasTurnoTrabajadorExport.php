6cfc8c3cbe4f1bd6897cbb4765554dfc4e192a74
Diego <diegomf.mendoza@gmail.com>
2019-12-26 10:46:00 -0500

    reparacion de reporte Marcas

--- app/Exports/MarcasTurnoTrabajadorExport.php
+++ app/Exports/MarcasTurnoTrabajadorExport.php
@@ -38,7 +38,7 @@ class MarcasTurnoTrabajadorExport implements FromView, WithColumnFormatting
     public function view(): View
     {
         $resultado=$resultado=Operador::join('marcador','operador.id','=','marcador.operador_id')
-            ->select('dni','nom_operador','ape_operador',DB::raw('GROUP_CONCAT(CONCAT_WS("@",marcador.ingreso,marcador.salida) ORDER BY marcador.ingreso ASC SEPARATOR "@") AS marcas'),DB::raw('ROUND(SUM(TIMESTAMPDIFF(MINUTE,marcador.ingreso,marcador.salida)/60 ),2) AS total'))
+            ->select('dni','nom_operador','ape_operador',DB::raw('GROUP_CONCAT(CONCAT_WS("@",marcador.ingreso,marcador.salida) ORDER BY marcador.ingreso ASC SEPARATOR "@") AS marcas'),DB::raw('ROUND(SUM(TIMESTAMPDIFF(MINUTE,marcador.ingreso,IF(marcador.salida is null,marcador.ingreso,marcador.salida))/60 ),2) AS total'))
             ->where('marcador.turno_id',$this->turno_id)
             ->groupBy('operador.dni')
             ->get();
