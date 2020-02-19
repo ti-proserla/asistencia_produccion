360711567c649a0a2f541724f8eb42239a65b8b3
Diego <diegomf.mendoza@gmail.com>
2020-02-19 09:29:45 -0500

    fecha ref

--- database/migrations/2020_02_18_175612_add_fecharef_to_marcador.php
+++ database/migrations/2020_02_18_175612_add_fecharef_to_marcador.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTurnoidToMarcador extends Migration
+class AddFecharefToMarcador extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTurnoidToMarcador extends Migration
     public function up()
     {
         Schema::table('marcador', function (Blueprint $table) {
-            $table->integer('turno_id')->default(0); 
+            $table->date('fecha_ref')->nullable(); 
         });
     }
 
