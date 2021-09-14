7fe84229eec48b4184765736f4c91beea6cfd634
Diego Mendoza <diegomf.mendoza@gmail.com>
2021-09-14 12:42:56 -0500

    etiquetas

--- resources/views/reporte.blade.php
+++ resources/views/reporte.blade.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reportes de conteo</title>
-    <link rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -14,7 +13,6 @@
     <link rel="stylesheet" href="{{ asset('css/dragon-desing.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fotocheck.css') }}">
     <link rel="stylesheet" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}">
-    <link href="https://unpkg.com/vue-croppa/dist/vue-croppa.min.css" rel="stylesheet" type="text/css">
     {{-- @laravelPWA --}}
 </head>
 <body>
