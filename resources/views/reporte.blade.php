5268cad95d66b063a5a40bcc964e4a6d8c754bbf
Diego Mendoza <diegomf.mendoza@gmail.com>
2021-09-14 12:40:24 -0500

    etiquetas

--- resources/views/reporte.blade.php
+++ resources/views/reporte.blade.php
@@ -9,6 +9,8 @@
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="{{ asset('css/vue-croppa.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vue-select.css') }}">
     <link rel="stylesheet" href="{{ asset('css/dragon-desing.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fotocheck.css') }}">
     <link rel="stylesheet" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}">
